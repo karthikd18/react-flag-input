999c556b09e0768798e14b7f227955adf72f9dc4
karthikd18 <56375192+karthikd18@users.noreply.github.com>
2023-06-29 00:33:04 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -23,21 +23,49 @@ export default function App() {
   const [inputChange, setInputChange] = useState(false);
 
   const [countryCode, setCountryCode] = useState();
-  const [currentCountry, setCurrentCountry] = useState();
+  const [countryCodeLength, setCountryCodeLength] = useState(
+    currItem.countryCode.length
+  );
+  const [currentCountry, setCurrentCountry] = useState(countries[0]);
 
   const [changeLanguage, setChangeLanguage] = useState(false);
 
   const [prefix, setPrefix] = useState(currItem.countryCode);
   useEffect(() => {
-    console.log('currentCountry', currentCountry);
-  }, [currentCountry]);
+    console.log('countryCodeLength', countryCodeLength);
+    countries?.map((item) => {
+      if (
+        item.countryCode.length > countryCodeLength &&
+        countryCodeLength !== item.countryCode.length
+      ) {
+        console.log('item.countryCode.length', item.countryCode.length);
+
+        setCountryCodeLength(item.countryCode.length);
+      }
+    });
+  }, []);
 
   useEffect(() => {
-    console.log('inputValue', inputValue);
+    console.log('inputValue---', inputValue, countryCodeLength);
     if (inputValue && inputValue !== currItem.countryCode) {
       console.log('inputValue111', inputValue.length);
       console.log('inputValue111', typeof inputValue);
-      if (inputValue?.length == 2) {
+      for (let i = 0; i < countryCodeLength; i++) {
+        if (inputValue?.length == i + 1) {
+          console.log('inputValue?.length == 2', inputValue, i);
+
+          let code = inputValue?.substring(0, i + 1);
+          console.log('countrycode', code);
+          let countrycode = countries.find((f) => {
+            return f.countryCode.includes(code);
+          });
+          console.log('countrycode', countrycode);
+          countrycode?.code && setCurrentCountry(countrycode);
+          countrycode?.code && setCountryCode(countrycode?.code);
+        }
+      }
+
+      /*  if (inputValue?.length == 2) {
         console.log('inputValue?.length == 2', inputValue);
 
         let code = inputValue?.substring(0, 3);
@@ -74,14 +102,25 @@ export default function App() {
         countrycode?.code && setCurrentCountry(countrycode);
 
         console.log('countrycode', countrycode);
-      }
+      } */
     }
   }, [inputValue]);
   const handleChangeLanguage = (item, e) => {
     console.log('handleChangeLanguage', item, e);
 
+    let codeCurrentLength = currentCountry?.countryCode?.length;
+    //let codeNewLength = currentCountry.countryCode.length
+    console.log('handleChangeLanguagecodeLength', codeCurrentLength);
+
+    let oldinputValue = inputValue.substring(
+      codeCurrentLength,
+      inputValue.length
+    );
+    console.log('handleChangeLanguagecodeLength', oldinputValue);
+
+    let value = item.countryCode + oldinputValue;
     setCountryCode(item.code);
-    setInputValue(item.countryCode);
+    setInputValue(value);
     setCurrentCountry(item);
   };
   const handleChangeInput = (e) => {
